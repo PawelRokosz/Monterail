6d519ba23af85ba0713407b0d3cbcfce55a1e43f
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-09-19 21:37:46 +0200

    profile section update

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -13,11 +13,17 @@ class Profile extends React.Component {
             </div>
             <div className="profile__info text__standard text__standard--bold">
               <div className="profile__info-text"><span className="profile__info-text-important text__upper text__upper--a">member for</span> 5 months</div>
-              <div className="profile__info-text"><span className="profile__info-text-important text__upper text__upper--a">last seen</span> saturday afternoon</div>
+              <div className="profile__info-text"><span className="profile__info-text-important text__upper text__upper--a">last seen</span> Saturday afternoon</div>
               <div className="profile__info-text"><span className="profile__info-text-important text__upper text__upper--a">activity level</span></div>
             </div>
           </header>
-          <div className="profile__main"></div>
+          <div className="profile__main">
+            <div className="profile__carousel">
+              <div className="carousel-left">l</div>
+              <div className=" text text__big">How it all started</div>
+              <div className="carousel-right">r</div>
+            </div>
+          </div>
           <footer className="profile__footer"></footer>
         </div>
       </div>
