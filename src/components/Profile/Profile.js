3cac00362095412accd170e2fd15a3c1f96bb38b
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-09-21 17:41:32 +0200

    profile ready

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,6 +6,7 @@ class Profile extends React.Component {
     return (
       <div className="profile">
         <div className="profile__container">
+          <div className="profile__close">x</div>
           <header className="profile__header">
             <div className="profile__user">
               <img className="profile__user-photo" src={photo} alt=""/>
@@ -23,8 +24,53 @@ class Profile extends React.Component {
               <div className=" text text__big">How it all started</div>
               <div className="carousel-right">r</div>
             </div>
+            <div className="profile__history">
+              <div className="profile__history-title text__upper text__upper--b">that's where we have been these 6 months ago</div>
+              <div className="profile__history-stats">
+                <div className="profile__history-stat profile__history-stat--1"><div><div className="text text__bold">46</div><div className="text__normal text__normal--italic">peers</div></div></div>
+                <div className="profile__history-stat profile__history-stat--2"><div><div className="text text__bold">29</div><div className="text__normal text__normal--italic">discussions</div></div></div>
+                <div className="profile__history-stat profile__history-stat--3"><div><div className="text text__bold">19</div><div className="text__normal text__normal--italic">findings</div></div></div>
+                <div className="profile__history-stat profile__history-stat--4"><div><div className="text text__bold">10</div><div className="text__normal text__normal--italic">questions</div></div></div>
+              </div>
+              <div className="profile__related-users">
+                <div className="profile__related-users-title text__upper text__upper--b">who joined the platform that same period</div>
+                <div className="profile__related-users-list">
+                  <div className="profile__related-users-user">
+                    <div className="profile__related-users-user-backgroud">
+                      <div className="profile__related-users-user-img"><img src={photo} alt=""/></div>
+                    </div>
+                    <div className="profile__related-users-name text__eyecatch text__eyecatch-b">SEN Waweru</div>
+                  </div>
+                  <div className="profile__related-users-user">
+                    <div className="profile__related-users-user-backgroud">
+                      <div className="profile__related-users-user-img"><img src={photo} alt="user"/></div>
+                    </div>
+                    <div className="profile__related-users-name text__eyecatch text__eyecatch-b">Patricia</div>
+                  </div>
+                  <div className="profile__related-users-user">
+                    <div className="profile__related-users-user-backgroud">
+                      <div className="profile__related-users-user-img"><img src={photo} alt=""/></div>
+                    </div>
+                    <div className="profile__related-users-name text__eyecatch text__eyecatch-b">Joseph Alouch</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <footer className="profile__footer"></footer>
+          <footer className="profile__footer">
+            <div className="profile__footer-title text__upper text__upper--b">the hottest discussions these days</div>
+            <div className="profile__footer-background">
+              <div className="profile__footer-img"><img src={photo} alt="user"/></div>
+              <div className="profile__footer-found"><span className="text__eyecatch text__eyecatch-b">Andrew</span> <span className="text__upper text__upper--b">found the guardian article</span></div>
+              <div className="profile__footer-title text__eyecatch text__eyecatch--c">Vegan diet to stop diabetes progress</div>
+              <div className="profile__footer-stats text__normal text__normal--italic">
+                <span className="text__bold"> 6 peers involved </span>
+                <span className="text__bold"> 3 related discussions </span>
+                <span className="text__bold"> 3 conversations </span>
+                <span className="text__bold"> 19 upvotes </span>
+              </div>
+          </div>
+          </footer>
         </div>
       </div>
     );
